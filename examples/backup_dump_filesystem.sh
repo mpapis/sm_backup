f6e2de4e50798a701b3f614c75b67e8ebf8fe8ba
mpapis <mpapis@gmail.com>
2011-07-27 20:33:17 +0200

    apply paths for new bdsm modules/extensions layout ... part 2

--- examples/backup_dump_filesystem.sh
+++ examples/backup_dump_filesystem.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bdsm
 
 #Load backup framework
-module_or_error "backup" \
+module_or_error "backup/scripting" \
   "backup extension not available, install it with:  bdsm extend mpapis/bdsm_backup backup"
 
 backup_dir ~/tmp/test
--- examples/backup_ssh_incr_filesystem.sh
+++ examples/backup_ssh_incr_filesystem.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bdsm
 
 #Load backup framework
-module_or_error "backup" \
+module_or_error "backup/scripting" \
   "backup extension not available, install it with:  bdsm extend mpapis/bdsm_backup backup"
 
 ssh_incr localhost ~/tmp/test3 2 ~/tmp/test1/
